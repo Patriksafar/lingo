b0b5e95c449e66b2e7c23774fd86fbee48f6f6f2
Patriksafar <patrik.safar@educanet.cz>
2024-12-14 13:40:47 +0100

    feat: shadcn init

--- src/components/auth/signin-button.tsx
+++ src/components/auth/signin-button.tsx
@@ -1,19 +1,10 @@
-import { signIn } from "@/auth";
+import Link from "next/link";
+import { Button } from "../ui/button";
 
 export function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn();
-      }}
-    >
-      <button
-        type="submit"
-        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-      >
-        Sign in
-      </button>
-    </form>
+    <Button type="submit" asChild>
+      <Link href="/login">Sign in</Link>
+    </Button>
   );
 }
