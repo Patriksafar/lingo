5308e6b0ff661cc9d784749cfb1a5ddfca88431d
Patriksafar <patrik.safar@educanet.cz>
2024-12-14 01:03:08 +0100

    feat: pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,21 +2,17 @@ import { auth } from "@/auth";
 import { SignIn } from "@/components/auth/signin-button";
 import { SignOut } from "@/components/auth/signout-button";
 import UserAvatar from "@/components/user-avatar/user-avatar";
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
+  if (!session) return redirect("/api/auth/signin");
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl sm:text-4xl font-bold text-center sm:text-left">
-          Welcome to Lingo!
-        </h1>
+        Protected dashboard with login only
         <UserAvatar />
-        <p className="font-[family-name:var(--font-geist-mono)]">
-          Get started by signing in
-        </p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           {!session ? <SignIn /> : <SignOut />}
         </div>
--- src/components/auth/signin-button.tsx
+++ src/components/auth/signin-button.tsx
@@ -8,7 +8,12 @@ export function SignIn() {
         await signIn();
       }}
     >
-      <button type="submit">Sign in</button>
+      <button
+        type="submit"
+        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+      >
+        Sign in
+      </button>
     </form>
   );
 }
