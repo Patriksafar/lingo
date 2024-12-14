45383035d851ad4348f7ae860bb23f798bd0c7fe
Patriksafar <patrik.safar@educanet.cz>
2024-12-14 14:26:06 +0100

    feat: session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@/auth";
 import { SignIn } from "@/components/auth/signin-button";
 import { SignOut } from "@/components/auth/signout-button";
-import UserAvatar from "@/components/user-avatar/user-avatar";
 import { Languages } from "lucide-react";
 
 export default async function Home() {
@@ -14,15 +13,24 @@ export default async function Home() {
           <Languages className="size-8" />
         </div>
         <h1 className="text-3xl sm:text-4xl font-bold text-center sm:text-left">
-          Welcome to Lingo!
+          Welcome to Lingo {session ? session.user?.name : ""}!
         </h1>
-        <UserAvatar />
-        <p className="font-[family-name:var(--font-geist-mono)]">
-          Get started by signing in
-        </p>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {!session ? <SignIn /> : <SignOut />}
-        </div>
+        {!session ? (
+          <>
+            <p className="font-[family-name:var(--font-geist-mono)]">
+              Get started by signing in
+            </p>
+            <div className="flex gap-4 items-center flex-col sm:flex-row">
+              <SignIn />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="flex gap-4 items-center flex-col sm:flex-row">
+              <SignOut />
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
